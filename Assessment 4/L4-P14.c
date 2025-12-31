e7f5dba7e8546fe78761af76632f148615385b85
yuvan66 <yuvanudt@gmail.com>
2025-12-31 11:06:17 +0530

    git commit -m "Added solutions"

---
+++ Assessment 4/L4-P14.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a,a1,b,c;
+    scanf("%d",&a);\
+    a1 = a;
+    b = a%10;
+    loop1: if (a!=0)
+    {
+        c += 1;
+        a = a/10;
+        goto loop1;
+    }
+    
+    printf("%d",c);
+    return 0;
+}
