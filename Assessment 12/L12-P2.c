1f65523a7a27c28871a48cd9049ac879612c181f
yuvan66 <yuvanudt@gmail.com>
2026-01-07 09:01:48 +0530

    Pointers

---
+++ Assessment 12/L12-P2.c
@@ -0,0 +1,28 @@
+/*Question: Get a string and a character from the user, find all the positions where the character is present, and print it.
+
+Example:
+
+string: helloetalvislearning
+
+character: e
+
+Answer: 2, 6, 14*/
+#include <stdio.h>
+#include <string.h>
+void findchar(char * name, char get ){
+    int i =0;
+    while(name[i]!='\0'){
+        if(name[i]==get){
+            printf("%d ",i+1);
+        }
+        i++;
+    }
+}
+int main(){
+    char name[30];
+    fgets(name,30,stdin);
+    name[strcspn(name,"\n")]='\0';
+    char get;
+    scanf("%c",&get);
+    findchar(name,get);
+}
